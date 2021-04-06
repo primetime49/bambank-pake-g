4fa90ee704185f9f74868732adad2dba620daa97
ramadhan.afwan <rama.afwan.mutasodirin3@gmail.com>
2021-04-07 06:53:28 +0700

    create homepage template

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,7 +20,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('home');
+		$this->load->view('welcome_message');
 	}
 
 	public function vaksin(){
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,115 +4,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<title>Maju Bersama Bambang</title>
 
 	<style type="text/css">
-
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
-
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
 	</style>
 </head>
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
-
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+	<?php $this->load->view('layout/header'); ?>
 </div>
 
-<footer id="footer-desktop">
-        <div class="container">
-            <div class="row py-3">
-            	<div class="col-md-7" style="padding-top:20px">
-            		<div class="d-lg-flex justify-content-between"></div>
-            	</div>
-                	<div class="col-md-4" style="padding-top:5px">
-				</div>
-			</div>
-
-            <hr>
-            	<div class="d-flex justify-content-between">
-
-                <div>
-					<p class="text-small text-muted">KONTAK INFORMASI</p>
-					<p class="text-small text-muted">POSKO TIM TANGGAP COVID-19</p>
-					<p class="text-small text-muted">Dinas Kesehatan DKI Jakarta</p>
-					<span><img src="https://corona.jakarta.go.id/img/logo/pin.png" alt=""> Jalan Kesehatan No. 10 Jakarta Pusat</span>
-					<br>
-					<span><img src="https://corona.jakarta.go.id/img/logo/phone-black.png" alt=""> 112, 081 112 112 112, atau 081 388 376 955</span>
-					<br>
-					<p class="text-small text-muted">Apabila terjadi gejala terkait COVID-19, harap segera periksa diri Anda.</p>
-                </div>
-
-            </div>
-
-        </div>
-</footer>
+<div>
+	<?php $this->load->view('layout/footer'); ?>
+</div>
 
 </body>
 </html>
