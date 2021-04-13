72ca777e45bf6d8c8a7fc0ea1046666ccf290eca
pandumaulana98 <pandu.maulana44@gmail.com>
2021-04-13 21:28:15 +0700

    footer baru

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,22 +1,61 @@
 <footer id="footer-desktop">
     <div class="container">
         <div class="row">
-			<div class="col align-self-start">
-				<p class="text-small text-muted" style="text-align : left">KONTAK INFORMASI</p>
-				<p class="text-small text-muted" style="text-align : left">POSKO TIM TANGGAP COVID-19</p>
-				<p class="text-small text-muted" style="text-align : left">Dinas Kesehatan DKI Jakarta</p>
-				<span style="float:left"><img src="https://corona.jakarta.go.id/img/logo/pin.png" alt=""> Jalan Kesehatan No. 10 Jakarta Pusat</span>
-				<br>
-				<span style="float:left"><img src="https://corona.jakarta.go.id/img/logo/phone-black.png" alt=""> 112, 081 112 112 112, atau 081 388 376 955</span>
-				<br>
-				<p class="text-small text-muted" style="text-align : left">Apabila terjadi gejala terkait COVID-19, harap segera periksa diri Anda.</p>
-            </div>
+			<nav class="navbar navbar-expand-lg navbar-dark>
+			<div class="container">
+				<a class="navbar-brand" href="<?= base_url('home') ?>">
+				<img src="<?= base_url('public/logo.png') ?>" alt="Logo" width="50" height="50">
+				</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				
+				<div class="collapse navbar-collapse" id="navbarNav">
+					<ul class="navbar-nav">
+						<li class="nav-item">
+							<a class="nav-link" href="<?= base_url('home') ?>">Home</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="<?= base_url('contact') ?>">Peta Sebaran</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="<?= base_url('faqs') ?>">Berita</a>
+						</li>
+							<li class="nav-item">
+							<a class="nav-link" href="<?= base_url('faqs') ?>">Peraturan</a>
+						</li>
+							<li class="nav-item">
+							<a class="nav-link" href="<?= base_url('faqs') ?>">Program</a>
+						</li>
+							<li class="nav-item">
+							<a class="nav-link" href="<?= base_url('vaksin') ?>">Vaksin</a>
+						</li>
+							<li class="nav-item">
+							<a class="nav-link" href="<?= base_url('faqs') ?>">Rumah Sakit</a>
+						</li>
+							<li class="nav-item">
+							<a class="nav-link" href="<?= base_url('faqs') ?>">Tes Covid</a>
+						</li>
+							<li class="nav-item">
+							<a class="nav-link" href="<?= base_url('faqs') ?>">Covid-19</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+
 			<div class="col align-self-end">
 				<p style="font-size:10px">Pengunjung Saat ini : <b>150</b> | Hari ini : <b>3,872</b> | Minggu ini : <b>9,570</b> | Total : <b>236,986</b></p>
 			</div>
 		</div>
     </div>
-     Copyright &copy; <?php echo date('Y'); ?>. Warganet.
+
+	<p style="font-size:20px">Layanan Darurat<br></p>
+	<span><img src="https://corona.jakarta.go.id/img/logo/pin.png" alt=""> Jalan Kesehatan No. 10 Jakarta Pusat<br></span>
+	<span><img src="https://corona.jakarta.go.id/img/logo/phone-black.png" alt=""> 112, 0811-1211-2112, atau 0813-8837-6955</span>
+
+	<hr>
+    Copyright &copy; <?php echo date('Y'); ?>. Warganet.
+	<br>	
 </footer>
 </body>
 </html>
