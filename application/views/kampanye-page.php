a58c4784d74738b53feade74ff545f79e7975885
ramadhan.afwan <rama.afwan.mutasodirin3@gmail.com>
2021-04-23 15:07:13 +0700

    fixing vaksinpage

---
+++ application/views/kampanye-page.php
@@ -0,0 +1,17 @@
+<!-- Dashboard divided in 3 section : Navbar, Header (Jumbotron), Data, Footer -->
+<!-- Reuse section-navbar in every page -->
+<section class="section-navbar">
+	<div>
+		<?php $this->load->view('layout/navbar'); ?>
+	</div>
+</section>
+
+
+
+<!-- Reuse section-footer in every page -->
+<section class="section-footer">
+	<hr>
+	<div>
+		<?php $this->load->view('layout/footer'); ?>
+	</div>
+</section>
