f66ff7c81cf9660eefa095a7c4a6ef4e5d276f95
Firzi Nabil <57490197+FoRiZon@users.noreply.github.com>
2021-05-05 13:28:27 +0700

    Halaman Regulasi + Jumbotron Image Fix

--- application/views/berita.php
+++ application/views/berita.php
@@ -8,8 +8,9 @@
 <!-- Only use on Dashboard page -->
 <section class="section-header">
   <div class="jumbotron jumbotron-fluid jumbo">
-    <h1 style="text-align : left;font-size:50px; width:500px; margin-left: 200px; margin-top: 50px; float: left">Berita Covid-19 di DKI Jakarta</h1>
-    <img src="https://www.pngkey.com/png/full/208-2088636_news-amp-events-report-icon-black-png-newspaper.png" style="height:200px; width:200px; float: right; ; margin-right: 250px">
+      <a href="<?= site_url('') ?>" style="margin-left: 40px; margin-top: -35px; float: left; color: white">Home > Berita </a>
+    <h1 style="text-align : left;font-size:50px; width:500px; margin-left: 250px; margin-top: 50px; float: left">Berita Covid-19 di DKI Jakarta</h1>
+    <img src="https://www.pngkey.com/png/full/208-2088636_news-amp-events-report-icon-black-png-newspaper.png" style="height:200px; width:200px; float: right; ; margin-right: 300px">
   </div>
 </section>
 
@@ -22,85 +23,95 @@
         </div>
         <div class="row justify-content-around data-row-1">
           <div class="col-sm" style="margin-left: 20px; margin-right: 20px">
-            
-            <img src="https://akcdn.detik.net.id/community/media/visual/2021/04/05/aktivis-antimasker-banyuwangi-terpapar-covid-19_43.jpeg?w=700&q=90" style="height:200px; font-weight: 10; width:400px; margin-top=25px; margin-bottom: 20px; float: left">
-            <h2 style="text-align : center;font-size:16px">Nasib Aktivis Antimasker di Tebet yang Kini Lemah Karena COVID-19</h2>
-            <p style="text-align : center;font-size:16px; font-weight: 10;">Selasa, 06 Apr 2021 07:55 WIB</p>
+             <div class="container data" style="height: 350px; width: 1100px; margin-top: 5px; border:1px solid #DFDFE5;">
+            <img src="https://akcdn.detik.net.id/community/media/visual/2021/04/05/aktivis-antimasker-banyuwangi-terpapar-covid-19_43.jpeg?w=700&q=90" style="height:200px; font-weight: 10; width:400px; margin-bottom: 20px; float: left; border-radius: 25px; margin-top: 20px">
+            <br><h2 style="text-align : center;font-size:24px ; margin-top: 10px">Nasib Aktivis Antimasker di Tebet yang Kini Lemah Karena COVID-19</h2>
+   
             <p style="text-align : justify; font-family:helvetica; font-weight: 10; margin-bottom: 20px; border-radius: 25px; margin-left: 420px">TEBET - Sudah jatuh tertimpa tangga. Mungkin pepatah ini bisa menggambarkan kisah M. Yunus Wahyudi, seorang aktivis antimasker di Tebet positif COVID-19. Diketahui Yunus juga terdakwa kasus penyebaran berita hoaks. Yunus ditahan polisi pada Rabu (14/10/2020) setelah menjadi tersangka kasus penyebaran hoaks di media sosial. Saat itu, Yunus menyebarkan kabar bahwa di jakarta tidak ada COVID-19.</p>
+                     <p style="text-align : left ;font-size:16px; font-weight: 10; margin-left: 420px">Selasa, 06 Apr 2021 07:55 WIB</p>
             <a href= "https://news.detik.com/berita-jawa-timur/d-5521525/nasib-aktivis-antimasker-di-banyuwangi-yang-kini-lemah-karena-covid-19" style="text-align : right; font-family:helvetica; font-weight: 10; float: right; margin-bottom: 20px; border-radius: 25px; margin-left: 25px">Read more at detik.com ></a>
             <p id="like" onclick="myFunction()" style="margin-left: 420px"> ❤️ 23 | 🔗 12 | 🔖 Bookmark </p>
+            <p style="text-align: center; margin-left: 200px">_ _ _ - _ _ </p>
+          </div>
           </div>  
         </div>
 
           <div class="col-auto">
         <div id="title-wording-data" style="background-color: #2073F3; color: white; margin-top: 15px; margin-bottom: 10px">
-          <h4 style="margin-top: 25px">All News Articles</h4>
-          <p style="margin-bottom: 20px">6 April 2021</p>
+          <p style="margin-bottom: 20px">Update Terakhir: 6 April 2021 15:00 WIB</p>
         </div>
 
-        <div class="row justify-content-around data-row-1" style="margin-left: 10px; margin-right: 10px">
+        <div class="row justify-content-around data-row-1">
           <div class="col detail-data" style="margin-left: 10px; margin-right: 10px; border:1px solid #DFDFE5;">
-            <h4 style="text-align : center;font-size:16px; height: 50px; margin-top: 10px">Nasib Aktivis Antimasker di Tebet yang Kini Lemah Karena COVID-19</h4>
-            <p style="text-align : center;font-size:16px; font-weight: 10;">Selasa, 06 Apr 2021 - 11:12 WIB</p>
+            
             <center>
-            <img src="https://akcdn.detik.net.id/community/media/visual/2021/04/05/aktivis-antimasker-banyuwangi-terpapar-covid-19_43.jpeg?w=700&q=90" style="height:150px; font-weight: 10; width:300px; margin-top=25px; margin-bottom: 20px"></center>
+            <img src="https://akcdn.detik.net.id/community/media/visual/2021/04/05/aktivis-antimasker-banyuwangi-terpapar-covid-19_43.jpeg" style="height:150px; font-weight: 10; width:300px; margin-top=25px; margin-bottom: 20px; border-radius: 25px; margin-top: 10px"></center>
+            <h3 style="text-align : center;font-size:16px; height: 50px; margin-top: 10px; margin-bottom: 10px">Nasib Aktivis Antimasker di Tebet yang Kini Lemah Karena COVID-19</h3>
+            <p style="text-align : justify; font-family:helvetica; font-weight: 10; margin-bottom: 20px">TEBET - Sudah jatuh tertimpa tangga. Mungkin pepatah ini bisa menggambarkan kisah M. Yunus Wahyudi, seorang aktivis antimasker di Tebet positif COVID-19. Diketahui Yunus juga terdakwa kasus penyebaran berita hoaks. Yunus ditahan polisi pada Rabu (14/10/2020) setelah menjadi tersangka kasus penyebaran hoaks di media sosial. Saat itu, Yunus menyebarkan kabar bahwa di jakarta tidak ada COVID-19.</p>
+            <p style="text-align : center;font-size:16px; font-weight: 10;">Selasa, 06 Apr 2021 - 11:12 WIB</p><hr>
             <p id="like2" onclick="myFunction()" style="text-align: center;"> ❤️ 23 | 🔗 12 | 🔖 Bookmark </p>
             <a href= "https://news.detik.com/berita-jawa-timur/d-5521525/nasib-aktivis-antimasker-di-banyuwangi-yang-kini-lemah-karena-covid-19" style="text-align : right; font-family:helvetica; font-weight: 10; float: right; margin-bottom: 20px; border-radius: 25px; margin-left: 25px">Read more at Detik.com ></a>
           
           </div>
   
           <div class="col detail-data" style="margin-left: 10px; margin-right: 10px; border:1px solid #DFDFE5;">
-            <h4 style="text-align : center;font-size:16px; height: 50px; margin-top: 10px">Kekisruhan Kedatangan Ratusan WN India, Masuk Indonesia untuk Hindari Tsunami Covid-19 di Negaranya</h4>
-            <p style="text-align : center;font-size:16px; font-weight: 10;">Selasa, 06 Apr 2021 - 10:10 WIB</p>
+        
             <center>
-            <img src="https://asset.kompas.com/crops/hW3icOn9VYatrWCsrtcDnCT5KnY=/0x0:798x532/750x500/data/photo/2021/04/23/608298997337c.jpg" style="height:150px; font-weight: 10; width:300px; margin-top=25px; margin-bottom: 20px"></center>
+            <img src="https://asset.kompas.com/crops/hW3icOn9VYatrWCsrtcDnCT5KnY=/0x0:798x532/750x500/data/photo/2021/04/23/608298997337c.jpg" style="height:150px; font-weight: 10; width:300px; margin-top: 10px ; margin-bottom: 20px; border-radius: 25px"></center>
+            <h3 style="text-align : center;font-size:16px; height: 50px; margin-top: 10px; margin-bottom: 10px">Kekisruhan Kedatangan Ratusan WN India, Masuk Indonesia untuk Hindari Tsunami Covid-19 di Negaranya</h3>
+            <p style="text-align : justify; font-family:helvetica; font-weight: 10; margin-bottom: 20px">JAKARTA - Sebanyak 117 warga negara India datang ke Indonesia melalui Bandara Soekarno-Hatta, Tangerang, pada Rabu (21/4/2021). Warga negara asing (WNA) itu datang ke Bandara Soekarno-Hatta, Kota Tangerang, dengan menggunakan pesawat Air Asia dengan nomor penerbangan XZ988. Imigrasi Bandara Soekarno-Hatta menyebut sejumlah WN India itu memiliki perizinan yang sesuai untuk masuk ke Indonesia.</p>
+            <p style="text-align : center;font-size:16px; font-weight: 10;">Selasa, 06 Apr 2021 - 10:10 WIB</p><hr>
             <p id="like3" onclick="myFunction2()" style="text-align: center;"> ❤️ 19 | 🔗 8 | 🔖 Bookmark </p>
-            <a href= "https://megapolitan.kompas.com/read/2021/04/24/11122221/kekisruhan-kedatangan-ratusan-wn-india-masuk-indonesia-untuk-hindari?page=all" style="text-align : right; font-family:helvetica; font-weight: 10; float: right; margin-bottom: 20px; border-radius: 25px; margin-left: 25px">Read more at Kompas.com ></a>
+            <a href= "https://megapolitan.kompas.com/read/2021/04/24/11122221/kekisruhan-kedatangan-ratusan-wn-india-masuk-indonesia-untuk-hindari?page=all" style="text-align : right; font-family:helvetica; font-weight: 10; float: right; margin-top: 10px; margin-bottom: 20px; border-radius: 25px; margin-left: 25px">Read more at Kompas.com ></a>
           </div>
           <div class="col detail-data" style="margin-left: 10px; margin-right: 10px; border:1px solid #DFDFE5;">
-            <h4 style="text-align : center;font-size:16px; height: 50px; margin-top: 10px">Jepang darurat COVID-19, Indonesia tetap bersiap untuk Olimpiade Tokyo</h4>
-            <p style="text-align : center;font-size:16px; font-weight: 10;">Selasa, 06 Apr 2021 - 07:55 WIB</p>
-            <center>
-            <img src="https://img.antaranews.com/cache/800x533/2021/04/19/43C93F64-4835-4589-9AF3-D0E1F4B18D08.jpeg" style="height:150px; font-weight: 10; width:300px; margin-top=25px; margin-bottom: 20px"></center>
-            <p id="like4" onclick="myFunction3()" style="text-align: center;"> ❤️ 16 | 🔗 6 | 🔖 Bookmark </p>
-            <a href= "https://www.antaranews.com/berita/2119170/jepang-darurat-covid-19-indonesia-tetap-bersiap-untuk-olimpiade-tokyo" style="text-align : right; font-family:helvetica; font-weight: 10; float: right; margin-bottom: 20px; border-radius: 25px; margin-left: 25px">Read more at AntaraNews.com ></a>
+          
+            <h4 style="text-align : center;font-size:16px; margin-top: 10px">Lokasi Berita</h4>
+            <p style="text-align : center;font-size:16px; font-weight: 10;">Dapatkan berita berdasarkan lokasi anda.</p>
+             <input style="margin-bottom: 20px; height: 60px; width: 300px" type="text" id="location" name="location" placeholder="Masukkan lokasi anda."><br>
+             <a href= "https://www.detik.com/ramadan/" style="text-align : center; font-family:helvetica; border:1px solid #000; height:30px; font-weight: 20; width:100px; float: right; margin-bottom: 20px; margin-top: 2px; border-radius: 10px; background-color: #2073F3; color: white; border:1px solid #FFFFFF;">Search</a>
+             <br><hr>
+                                    <H3> Terpopuler</H3><br>
+             <ol>
+  <li><a href="https://news.detik.com/berita-jawa-timur/d-5521525/nasib-aktivis-antimasker-di-banyuwangi-yang-kini-lemah-karena-covid-19" style="color: black">Nasib Aktivis Antimasker di Tebet yang Kini Lemah Karena COVID-19</a></li><hr>
+  <li><a href="https://megapolitan.kompas.com/read/2021/04/24/11122221/kekisruhan-kedatangan-ratusan-wn-india-masuk-indonesia-untuk-hindari?page=all" style="color: black">Kekisruhan Kedatangan Ratusan WN India, Masuk Indonesia untuk Hindari Tsunami Covid-19 di Negaranya</a></li><hr>
+  <li><a href="https://www.antaranews.com/berita/2084198/dinkes-jakarta-utara-siagakan-personel-bantu-belajar-tatap-muka" style="color: black">Dinkes Jakarta Utara siagakan personel bantu belajar tatap muka</a></li><hr>
+  <li><a href="https://www.republika.co.id/berita/qr520f467/menko-polhukam-mari-ambil-hikmah-dari-musibah-covid19" style="color: black">Menko Polhukam: Mari Ambil Hikmah dari Musibah Covid-19</a></li><hr>
+  <li><a href="https://www.suara.com/news/2021/04/06/115048/tinggal-1442-rsd-wisma-atlet-makin-berkurang-dihuni-pasien-covid-19" style="color: black">Tinggal 1.442, RSD Wisma Atlet Makin Berkurang Dihuni Pasien Covid-19</a></li>
+</ol>
           </div>
-        </div> 
-
-         <div class="col-auto">
-        <div id="title-wording-data" style="background-color: #2073F3; color: white; margin-top: 15px; margin-bottom: 10px">
-          <p style=" margin-top: 25px; margin-bottom: 20px">5 April 2021</p>
-        </div>
+        </div> <br>
   
         </div> 
-        <div class="row justify-content-around data-row-1" style="margin-left: 10px; margin-right: 10px">
-          <div class="col detail-data" style="margin-left: 10px; margin-right: 10px; border:1px solid #DFDFE5;">
-            <h4 style="text-align : center;font-size:16px; height: 50px; margin-top: 10px">Dinkes Jakarta Utara siagakan personel bantu belajar tatap muka</h4>
-            <p style="text-align : center;font-size:16px; font-weight: 10;">Senin, 05 Apr 2021 17.48 WIB</p>
+        <div class="row justify-content-around data-row-1">
+          <div class="col detail-data" style="margin-left: 10px; margin-right: 10px; border:1px solid #DFDFE5">
+           
             <center>
-            <img src="https://img.antaranews.com/cache/800x533/2021/03/30/antarafoto-simulasi-sekolah-hibrid-300321-adm-2.jpg" style="height:150px; font-weight: 10; width:300px; margin-top=25px; margin-bottom: 20px"></center>
+            <img src="https://img.antaranews.com/cache/800x533/2021/03/30/antarafoto-simulasi-sekolah-hibrid-300321-adm-2.jpg" style="height:150px; font-weight: 10; width:300px; margin-top:10px ; margin-bottom: 20px; border-radius: 25px"></center>
+             <h4 style="text-align : center;font-size:16px; height: 50px; margin-top: 10px; margin-bottom: 10px; border-radius: 25px">Dinkes Jakarta Utara siagakan personel bantu belajar tatap muka</h4>
+             <p style="text-align : justify; font-family:helvetica; font-weight: 10; margin-bottom: 20px">JAKARTA - Suku Dinas Kesehatan (Dinkes) Kota Jakarta Utara (Jakut) menyiagakan personel untuk membantu proses uji coba pembelajaran tatap muka di sekolah Jakarta Utara."(Personel yang turun) dari Puskesmas ada satu tim terdiri dari dokter, perawat dan pengemudi serta mobil ambulans siaga," kata Kepala Suku Dinas Kesehatan Jakarta Utara dr Yudi Dimyati, di Jakarta, Selasa.Yudi juga menyampaikan bahwa ada tugas dan wewenang dinas kesehatan provinsi atau kabupaten/kota dalam persiapan pembukaan sekolah tatap muka.</p>
+             <p style="text-align : center;font-size:16px; font-weight: 10;">Senin, 05 Apr 2021 17.48 WIB</p><hr>
             <p id="like5" onclick="myFunction4()" id="like" onclick="myFunction()" style="text-align: center;"> ❤️ 22 | 🔗 11 | 🔖 Bookmark </p>
             <a href= "https://www.antaranews.com/berita/2084198/dinkes-jakarta-utara-siagakan-personel-bantu-belajar-tatap-muka" style="text-align : right; font-family:helvetica; font-weight: 10; float: right; margin-bottom: 20px; border-radius: 25px; margin-left: 25px">Read more at AntaraNews.com ></a>
           
           </div>
   
           <div class="col detail-data" style="margin-left: 10px; margin-right: 10px; border:1px solid #DFDFE5;">
-            <h4 style="text-align : center;font-size:16px; height: 50px; margin-top: 10px">Menko Polhukam: Mari Ambil Hikmah dari Musibah Covid-19</h4>
-            <p style="text-align : center;font-size:16px; font-weight: 10;">Senin, 05 Apr 2021 15:06 WIB</p>
+            
             <center>
-            <img src="https://akcdn.detik.net.id/visual/2019/12/12/571f9000-abd2-48fa-bc9b-79a42ebcee9a_169.jpeg" style="height:150px; font-weight: 10; width:300px; margin-top=25px; margin-bottom: 20px"></center>
+            <img src="https://akcdn.detik.net.id/visual/2019/12/12/571f9000-abd2-48fa-bc9b-79a42ebcee9a_169.jpeg" style="height:150px; font-weight: 10; width:300px; margin-top:10px; margin-bottom: 20px; border-radius: 25px"></center>
+            <h4 style="text-align : center;font-size:16px; height: 50px; margin-top: 10px; margin-bottom: 10px">Menko Polhukam: Mari Ambil Hikmah dari Musibah Covid-19</h4>
+              <p style="text-align : justify; font-family:helvetica; font-weight: 10; margin-bottom: 20px">JAKARTA - Menko Polhukam menilai acara Tokoh Perubahan Republika memberikan inspirasi bagi masyarakat di tengah pandemi Covid-19. Mahfud menilai acara tersebut menjadi refleksi serta penghayatan bagi masyarakat untuk bersama-sama menghadapi pandemi Covid-19. Terlebih, mereka yang mendapatkan penghargaan menjadi contoh untuk tetap berjuang melawan Covid-19. Mahfud berharap masyarakat dapat mengambil hikmah dari musibah wabah Covid-19.</p>
+            <p style="text-align : center;font-size:16px; font-weight: 10;">Senin, 05 Apr 2021 15:06 WIB</p><hr>
             <p id="like6" onclick="myFunction5()" style="text-align: center;"> ❤️ 20 | 🔗 12 | 🔖 Bookmark </p>
             <a href= "https://www.republika.co.id/berita/qr520f467/menko-polhukam-mari-ambil-hikmah-dari-musibah-covid19" style="text-align : right; font-family:helvetica; font-weight: 10; float: right; margin-bottom: 20px; border-radius: 25px; margin-left: 25px">Read more at Republika.com ></a>
           </div>
-          <div class="col detail-data" style="margin-left: 10px; margin-right: 10px; border:1px solid #DFDFE5;">
-            <h4 style="text-align : center;font-size:16px; height: 50px; margin-top: 10px">Tinggal 1.442, RSD Wisma Atlet Makin Berkurang Dihuni Pasien Covid-19</h4>
-            <p style="text-align : center;font-size:16px; font-weight: 10;">Selasa, 05 Apr 2021 04.19 WIB</p>
-            <center>
-            <img src="https://media.suara.com/pictures/653x366/2021/02/28/48832-wisma-atlet.jpg" style="height:150px; font-weight: 10; width:300px; margin-top=25px; margin-bottom: 20px"></center>
-            <p id="like7" onclick="myFunction6()" style="text-align: center;"> ❤️ 16 | 🔗 5 | 🔖 Bookmark </p>
-            <a href= "https://www.suara.com/news/2021/04/06/115048/tinggal-1442-rsd-wisma-atlet-makin-berkurang-dihuni-pasien-covid-19" style="text-align : right; font-family:helvetica; font-weight: 10; float: right; margin-bottom: 20px; border-radius: 25px; margin-left: 25px">Read more at Suara.com ></a>
+          <div class="col detail-data" style="margin-left: 10px; margin-right: 10px; border:0px solid #FFFFFF; height: 1px">
+
           </div>
         </div> 
+
+
 
         <br>
 
