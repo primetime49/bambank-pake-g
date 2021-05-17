75f3f7b837211ff2a9809242f56b12abb28fc128
Adityo <adityo.anggraito@gmail.com>
2021-05-17 09:26:23 +0700

    merge regulasi

--- application/views/regulasi.php
+++ application/views/regulasi.php
@@ -17,104 +17,77 @@
   <div class="container data">
     <div class="row justify-content-md-center">
       <div class="col-auto">
-        <div id="title-wording-data" style="background-color: #2073F3; color: white; margin-top: 15px; margin-bottom: 10px">
-          <a>Lihat dan unduh infografik seputar Covid-19 di Jakarta.</a><br>
-          <a style="margin-top: 5px">Infografik yang ditampilkan berdasarkan informasi resmi dari Pemprov DKI Jakarta.</a>
-        </div> 
-        <div id="title-wording-data" style="background-color: #2073F3; color: white; margin-top: 15px; margin-bottom: 10px; height: 40px">
-          <h3>Infografik</h3>
-        </div>
-       
+        <center>
+          <br><br><u style="color: blue; font-size: 15">Infografik</u>
+          <a>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;</a>
+          <a style="color: lightblue; font-size: 15">Dokumen</a><br><br>
+      </center>
         <div class="row justify-content-around data-row-1">
              <div class="col-sm" style="margin-left: 20px; margin-right: 20px">
            <div class="container data" style="margin-left: 10px; margin-right: 10px; margin-top: 7px; border:1px solid #DFDFE5; width: 300px; float: left;">
             <center>
+              <h4 style="text-align : center;font-size:24px; margin-top: 10px">Yang dilarang & Tidak saat PSBB</h4><hr>
             <img src="https://thesmartlocal.com/indonesia/wp-content/uploads/2020/04/CNN-Infographic-PSBB-regions-in-Indonesia.jpg" style="height:280px; font-weight: 10; width:200px; margin-top: 25px; margin-bottom: 20px"></center>
-       
-            <h2 style="text-align : center;font-size:24px; margin-top: 10px">Yang dilarang & Tidak saat PSBB</h2><hr>
+                  
            <center>
-             <a href= "https://thesmartlocal.com/indonesia/psbb-regions-indonesia/" style="text-align : center; border:1px solid #000; height:30px; width:200px; font-weight: 25; font-family:helvetica; margin-bottom: 20px; margin-left: 10px; border-radius: 10px; background-color: #2073F3; color: white">Unduh PDF</a>
-            <h7 style="text-align : center; border:1px solid #000; font-family:helvetica; height:50px; width:200px; font-weight: 25; border-radius: 10px; background-color: #2073F3; "><a href= "https://thesmartlocal.com/indonesia/wp-content/uploads/2020/04/CNN-Infographic-PSBB-regions-in-Indonesia.jpg" style="color: white;"> Unduh JPG</a></h7><br>
-            <img src="https://socialb.co.uk/wp-content/uploads/2017/04/Twitter_logo_bird_transparent_png-1024x831.png" style="height:25px;  width:30px; margin-top: 10px; margin-bottom: 20px">
-            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/8/8c/Facebook_Home_logo_old.svg/798px-Facebook_Home_logo_old.svg.png" style="height:30px;  width:30px; margin-top: 10px; margin-bottom: 20px">
-            <img src="https://clipground.com/images/whatsapp-logo-transparent-png-9.png" style="height:30px;  width:30px; margin-top: 10px; margin-bottom: 20px">
-            <img src="http://www.clipartbest.com/cliparts/9cp/xEa/9cpxEanMi.png" style="height:30px;  width:30px; margin-top: 10px; margin-bottom: 20px">
-            </center>
+             <a href= "https://thesmartlocal.com/indonesia/psbb-regions-indonesia/" style="text-align : center; border:1px solid #000; height:30px; width:200px; font-weight: 25; font-family:helvetica; margin-bottom: 20px; margin-left: 10px; border-radius: 10px; color: black">Unduh PDF</a>
+            <h7 style="text-align : center; border:1px solid #000; font-family:helvetica; height:50px; width:200px; font-weight: 25; border-radius: 10px; margin-bottom: 40px"><a href= "https://thesmartlocal.com/indonesia/wp-content/uploads/2020/04/CNN-Infographic-PSBB-regions-in-Indonesia.jpg" style="color: black"> Unduh JPG</a></h7><br>
+                       </center>
           </div>
         
            <div class="container data" style="margin-left: 10px; margin-right: 10px; margin-top: 7px; border:1px solid #DFDFE5; width: 300px; float: left">
             <center>
+              <h4 style="text-align : center;font-size:24px; margin-top: 10px">Cara Memakai Masker yang Benar</h4><hr>
             <img src="https://cdn-2.tstatic.net/tribunnews/foto/bank/originals/cara-memakai-masker-yang-benar.jpg" style="height:280px; font-weight: 10; width:200px; margin-top: 25px; margin-bottom: 20px"></center>
        
-            <h2 style="text-align : center;font-size:24px; margin-top: 10px">Cara Memakai Masker yang Benar</h2><hr>
-           <center>
-             <a href= "https://palu.tribunnews.com/2020/03/28/lawan-virus-corona-ini-kumpulan-gambar-poster-pencegahan-covid-19-yang-bisa-dibagi-ke-media-sosial?page=2" style="text-align : center; border:1px solid #000; height:30px; width:200px; font-weight: 25; font-family:helvetica; margin-bottom: 20px; margin-left: 10px; border-radius: 10px; background-color: #2073F3; color: white;">Unduh PDF</a>
-            <h7 style="text-align : center; border:1px solid #000; font-family:helvetica; height:50px; width:200px; font-weight: 25; border-radius: 10px; background-color: #2073F3; "><a href= "https://cdn-2.tstatic.net/tribunnews/foto/bank/originals/cara-memakai-masker-yang-benar.jpg" style="color: white;"> Unduh JPG</a></h7><br>
-            <img src="https://socialb.co.uk/wp-content/uploads/2017/04/Twitter_logo_bird_transparent_png-1024x831.png" style="height:25px;  width:30px; margin-top: 10px; margin-bottom: 20px">
-            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/8/8c/Facebook_Home_logo_old.svg/798px-Facebook_Home_logo_old.svg.png" style="height:30px;  width:30px; margin-top: 10px; margin-bottom: 20px">
-            <img src="https://clipground.com/images/whatsapp-logo-transparent-png-9.png" style="height:30px;  width:30px; margin-top: 10px; margin-bottom: 20px">
-            <img src="http://www.clipartbest.com/cliparts/9cp/xEa/9cpxEanMi.png" style="height:30px;  width:30px; margin-top: 10px; margin-bottom: 20px">
-            </center>
+                       <center>
+             <a href= "https://palu.tribunnews.com/2020/03/28/lawan-virus-corona-ini-kumpulan-gambar-poster-pencegahan-covid-19-yang-bisa-dibagi-ke-media-sosial?page=2" style="text-align : center; border:1px solid #000; height:30px; width:200px; font-weight: 25; font-family:helvetica; margin-bottom: 20px; margin-left: 10px; border-radius: 10px; color: black">Unduh PDF</a>
+            <h7 style="text-align : center; border:1px solid #000; font-family:helvetica; height:50px; width:200px; font-weight: 25; border-radius: 10px;  margin-bottom: 40px"><a href= "https://cdn-2.tstatic.net/tribunnews/foto/bank/originals/cara-memakai-masker-yang-benar.jpg" style="color: black"> Unduh JPG</a></h7><br>
+                       </center>
           </div>
 
                     <div class="container data" style="margin-left: 10px; margin-right: 10px; margin-top: 7px; border:1px solid #DFDFE5; width: 300px; float: left">
             <center>
+              <h4 style="text-align : center;font-size:24px; margin-top: 10px">Novel Coronavirus (Covid-19)</h4><hr>
             <img src="https://cdn-2.tstatic.net/tribunnews/foto/bank/originals/seputar-corona-virus.jpg" style="height:280px; font-weight: 10; width:200px; margin-top: 25px; margin-bottom: 20px"></center>
-       
-            <h2 style="text-align : center;font-size:24px; margin-top: 10px">Novel Coronavirus (Covid-19)</h2><hr>
+                  
            <center>
-             <a href= "https://palu.tribunnews.com/2020/03/28/lawan-virus-corona-ini-kumpulan-gambar-poster-pencegahan-covid-19-yang-bisa-dibagi-ke-media-sosial?page=2" style="text-align : center; border:1px solid #000; height:30px;width:200px; font-weight: 25; font-family:helvetica; margin-bottom: 20px; margin-left: 10px; border-radius: 10px; background-color: #2073F3; color: white; box-sizing: content-box; ">Unduh PDF</a>
-            <h7 style="text-align : center; border:1px solid #000; font-family:helvetica; height:50px; width:200px; font-weight: 25; border-radius: 10px; background-color: #2073F3; box-sizing: content-box;"><a href= "https://cdn-2.tstatic.net/tribunnews/foto/bank/originals/seputar-corona-virus.jpg" style="color: white;"> Unduh JPG</a></h7><br>
-            <img src="https://socialb.co.uk/wp-content/uploads/2017/04/Twitter_logo_bird_transparent_png-1024x831.png" style="height:25px;  width:30px; margin-top: 10px; margin-bottom: 20px">
-            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/8/8c/Facebook_Home_logo_old.svg/798px-Facebook_Home_logo_old.svg.png" style="height:30px;  width:30px; margin-top: 10px; margin-bottom: 20px">
-            <img src="https://clipground.com/images/whatsapp-logo-transparent-png-9.png" style="height:30px;  width:30px; margin-top: 10px; margin-bottom: 20px">
-            <img src="http://www.clipartbest.com/cliparts/9cp/xEa/9cpxEanMi.png" style="height:30px;  width:30px; margin-top: 10px; margin-bottom: 20px">
-            </center>
+             <a href= "https://palu.tribunnews.com/2020/03/28/lawan-virus-corona-ini-kumpulan-gambar-poster-pencegahan-covid-19-yang-bisa-dibagi-ke-media-sosial?page=2" style="text-align : center; border:1px solid #000; height:30px;width:200px; font-weight: 25; font-family:helvetica; margin-bottom: 20px; margin-left: 10px; border-radius: 10px; box-sizing: content-box; color: black">Unduh PDF</a>
+            <h7 style="text-align : center; border:1px solid #000; font-family:helvetica; height:50px; width:200px; font-weight: 25; border-radius: 10px; box-sizing: content-box; margin-bottom: 40px"><a href= "https://cdn-2.tstatic.net/tribunnews/foto/bank/originals/seputar-corona-virus.jpg" style="color: black"> Unduh JPG</a></h7><br>
+                      </center>
           </div><br>
 
                  <div class="container data" style="margin-left: 10px; margin-right: 10px; margin-top: 7px; border:1px solid #DFDFE5; width: 300px; float: left;">
             <center>
+              <h4 style="text-align : center;font-size:24px; margin-top: 10px">Hindari Tiga Tempat Ini karena Covid-19</h4><hr>
             <img src="https://www.who.int/images/default-source/searo---images/countries/indonesia/covid19/avoid-3-places-poster-akb---bi.png?sfvrsn=a880b16a_2" style="height:280px; font-weight: 10; width:200px; margin-top: 25px; margin-bottom: 20px"></center>
        
-            <h2 style="text-align : center;font-size:24px; margin-top: 10px">Hindari Tiga Tempat Ini karena Covid-19</h2><hr>
-           <center>
-             <a href= "https://www.who.int/indonesia/news/novel-coronavirus/new-infographics/new-normal" style="text-align : center; border:1px solid #000; height:30px; width:200px; font-weight: 25; font-family:helvetica; margin-bottom: 20px; margin-left: 10px; border-radius: 10px; background-color: #2073F3; color: white;">Unduh PDF</a>
-            <h7 style="text-align : center; border:1px solid #000; font-family:helvetica; height:50px; width:200px; font-weight: 25; border-radius: 10px; background-color: #2073F3; "><a href= "https://www.who.int/images/default-source/searo---images/countries/indonesia/covid19/avoid-3-places-poster-akb---bi.png?sfvrsn=a880b16a_2" style="color: white;"> Unduh JPG</a></h7><br>
-            <img src="https://socialb.co.uk/wp-content/uploads/2017/04/Twitter_logo_bird_transparent_png-1024x831.png" style="height:25px;  width:30px; margin-top: 10px; margin-bottom: 20px">
-            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/8/8c/Facebook_Home_logo_old.svg/798px-Facebook_Home_logo_old.svg.png" style="height:30px;  width:30px; margin-top: 10px; margin-bottom: 20px">
-            <img src="https://clipground.com/images/whatsapp-logo-transparent-png-9.png" style="height:30px;  width:30px; margin-top: 10px; margin-bottom: 20px">
-            <img src="http://www.clipartbest.com/cliparts/9cp/xEa/9cpxEanMi.png" style="height:30px;  width:30px; margin-top: 10px; margin-bottom: 20px">
-            </center>
+              <center>
+             <a href= "https://www.who.int/indonesia/news/novel-coronavirus/new-infographics/new-normal" style="text-align : center; border:1px solid #000; height:30px; width:200px; font-weight: 25; font-family:helvetica; margin-bottom: 20px; margin-left: 10px; border-radius: 10px; color: black">Unduh PDF</a>
+            <h7 style="text-align : center; border:1px solid #000; font-family:helvetica; height:50px; width:200px; font-weight: 25; border-radius: 10px; margin-bottom: 40px"><a href= "https://www.who.int/images/default-source/searo---images/countries/indonesia/covid19/avoid-3-places-poster-akb---bi.png?sfvrsn=a880b16a_2" style="color: black"> Unduh JPG</a></h7><br>
+                        </center>
           </div>
         
            <div class="container data" style="margin-left: 10px; margin-right: 10px; margin-top: 7px; border:1px solid #DFDFE5; width: 300px; float: left">
             <center>
+              <h4 style="text-align : center;font-size:24px; margin-top: 10px">Cara Mengenakan Masker Medis</h4><hr>
             <img src="https://www.who.int/images/default-source/searo---images/countries/indonesia/covid19/mask-infographic-epi-win---bi.tmb-1920v.png?sfvrsn=77fbf4e9_1" style="height:280px; font-weight: 10; width:200px; margin-top: 25px; margin-bottom: 20px"></center>
        
-            <h2 style="text-align : center;font-size:24px; margin-top: 10px">Cara Mengenakan Masker Medis</h2><hr>
-           <center>
-             <a href= "https://www.who.int/indonesia/news/novel-coronavirus" style="text-align : center; border:1px solid #000; height:30px; width:200px; font-weight: 25; font-family:helvetica; margin-bottom: 20px; margin-left: 10px; border-radius: 10px; background-color: #2073F3; color: white;">Unduh PDF</a>
-            <h7 style="text-align : center; border:1px solid #000; font-family:helvetica; height:50px; width:200px; font-weight: 25; border-radius: 10px; background-color: #2073F3; "><a href= "https://www.who.int/images/default-source/searo---images/countries/indonesia/covid19/mask-infographic-epi-win---bi.tmb-1920v.png?sfvrsn=77fbf4e9_1" style="color: white;"> Unduh JPG</a></h7><br>
-            <img src="https://socialb.co.uk/wp-content/uploads/2017/04/Twitter_logo_bird_transparent_png-1024x831.png" style="height:25px;  width:30px; margin-top: 10px; margin-bottom: 20px">
-            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/8/8c/Facebook_Home_logo_old.svg/798px-Facebook_Home_logo_old.svg.png" style="height:30px;  width:30px; margin-top: 10px; margin-bottom: 20px">
-            <img src="https://clipground.com/images/whatsapp-logo-transparent-png-9.png" style="height:30px;  width:30px; margin-top: 10px; margin-bottom: 20px">
-            <img src="http://www.clipartbest.com/cliparts/9cp/xEa/9cpxEanMi.png" style="height:30px;  width:30px; margin-top: 10px; margin-bottom: 20px">
-            </center>
+            <center>
+             <a href= "https://www.who.int/indonesia/news/novel-coronavirus" style="text-align : center; border:1px solid #000; height:30px; width:200px; font-weight: 25; font-family:helvetica; margin-bottom: 20px; margin-left: 10px; border-radius: 10px; color: black">Unduh PDF</a>
+            <h7 style="text-align : center; border:1px solid #000; font-family:helvetica; height:50px; width:200px; font-weight: 25; border-radius: 10px; margin-bottom: 40px"><a href= "https://www.who.int/images/default-source/searo---images/countries/indonesia/covid19/mask-infographic-epi-win---bi.tmb-1920v.png?sfvrsn=77fbf4e9_1" style="color: black"> Unduh JPG</a></h7><br>
+                        </center>
           </div>
 
                     <div class="container data" style="margin-left: 10px; margin-right: 10px; margin-top: 7px; border:1px solid #DFDFE5; width: 300px; float: left">
             <center>
+              <h4 style="text-align : center;font-size:24px; margin-top: 10px">Jaga Diri dari Keluarga Anda</h4><hr>
             <img src="https://www.angkasapura2.co.id/data/images_cache/corcomm2/germas1.jpg" style="height:280px; font-weight: 10; width:200px; margin-top: 25px; margin-bottom: 20px"></center>
        
-            <h2 style="text-align : center;font-size:24px; margin-top: 10px">Jaga Diri dari Keluarga Anda</h2><hr>
-           <center>
-             <a href= "https://homposter.blogspot.com/2020/10/25-best-gambar-poster-tema-hidup-sehat.html" style="text-align : center; border:1px solid #000; height:30px; width:200px; font-weight: 25; font-family:helvetica; margin-bottom: 20px; margin-left: 10px; border-radius: 10px; background-color: #2073F3; color: white; box-sizing: content-box;">Unduh PDF</a>
-            <h7 style="text-align : center; border:1px solid #000; font-family:helvetica; height:200px; width:200px; font-weight: 25; border-radius: 10px; background-color: #2073F3; box-sizing: content-box;"><a href= "https://www.angkasapura2.co.id/data/images_cache/corcomm2/germas1.jpg" style="color: white;"> Unduh JPG</a></h7><br>
-            <img src="https://socialb.co.uk/wp-content/uploads/2017/04/Twitter_logo_bird_transparent_png-1024x831.png" style="height:25px;  width:30px; margin-top: 10px; margin-bottom: 20px">
-            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/8/8c/Facebook_Home_logo_old.svg/798px-Facebook_Home_logo_old.svg.png" style="height:30px;  width:30px; margin-top: 10px; margin-bottom: 20px">
-            <img src="https://clipground.com/images/whatsapp-logo-transparent-png-9.png" style="height:30px;  width:30px; margin-top: 10px; margin-bottom: 20px">
-            <img src="http://www.clipartbest.com/cliparts/9cp/xEa/9cpxEanMi.png" style="height:30px;  width:30px; margin-top: 10px; margin-bottom: 20px">
-            </center>
+              <center>
+             <a href= "https://homposter.blogspot.com/2020/10/25-best-gambar-poster-tema-hidup-sehat.html" style="text-align : center; border:1px solid #000; height:30px; width:200px; font-weight: 25; font-family:helvetica; margin-bottom: 20px; margin-left: 10px; border-radius: 10px; box-sizing: content-box; color: black">Unduh PDF</a>
+            <h7 style="text-align : center; border:1px solid #000; font-family:helvetica; height:200px; width:200px; font-weight: 25; border-radius: 10px;  box-sizing: content-box; margin-bottom: 40px"><a href= "https://www.angkasapura2.co.id/data/images_cache/corcomm2/germas1.jpg" style="color: black"> Unduh JPG</a></h7><br>
+                        </center>
           </div>
           </div>
         </div>
